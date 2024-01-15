fdc9965990c53f1f4369bc865e08170c6776778f
FelipeCN <felipecaninn14@gmail.com>
2024-01-15 12:04:51 -0300

    Refactor sideItems in DashboardLayout

--- src/app/portal/layout.tsx
+++ src/app/portal/layout.tsx
@@ -9,56 +9,63 @@ import {
 } from "react-icons/hi";
 import { MdOutlineDashboard } from "react-icons/md";
 
-export const sideItems: SideItems[] = [
-	{ label: "Dashboard", href: "/portal/dashboard", icon: MdOutlineDashboard },
-	{
-		label: "Usuario",
-		href: "/portal/usuarios",
-		icon: HiOutlineUser,
-		submenu: true,
-		subMenuItems: [
-			{
-				label: "Listagem",
-				href: "/portal/usuarios/listagem",
-				icon: HiDocumentText,
-			},
-		],
-	},
-	{
-		label: "Nfe",
-		href: "/portal/nfe",
-		icon: HiOutlineDocumentText,
-		submenu: true,
-		subMenuItems: [
-			{ label: "Notas", href: "/portal/nfe/emitidas", icon: HiDocumentText },
-			{ label: "Upload", href: "/portal/nfe/upload", icon: HiCloudUpload },
-		],
-	},
-	{
-		label: "Empresas",
-		href: "/portal/empresas",
-		icon: HiOutlineDocumentText,
-		submenu: true,
-		subMenuItems: [
-			{
-				label: "Listagem",
-				href: "/portal/empresas/listagem",
-				icon: HiDocumentText,
-			},
-			{
-				label: "Cadastro",
-				href: "/portal/empresas/cadastro",
-				icon: HiCloudUpload,
-			},
-		],
-	},
-];
-
 export default function DashboardLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	const sideItems: SideItems[] = [
+		{ label: "Dashboard", href: "/portal/dashboard", icon: MdOutlineDashboard },
+		{
+			label: "Usuario",
+			href: "/portal/usuarios",
+			icon: HiOutlineUser,
+			submenu: true,
+			subMenuItems: [
+				{
+					label: "Listagem",
+					href: "/portal/usuarios/listagem",
+					icon: HiDocumentText,
+				},
+			],
+		},
+		{
+			label: "Nfe",
+			href: "/portal/nfe",
+			icon: HiOutlineDocumentText,
+			submenu: true,
+			subMenuItems: [
+				{
+					label: "Notas",
+					href: "/portal/nfe/emitidas",
+					icon: HiDocumentText,
+				},
+				{
+					label: "Upload",
+					href: "/portal/nfe/upload",
+					icon: HiCloudUpload,
+				},
+			],
+		},
+		{
+			label: "Empresas",
+			href: "/portal/empresas",
+			icon: HiOutlineDocumentText,
+			submenu: true,
+			subMenuItems: [
+				{
+					label: "Listagem",
+					href: "/portal/empresas/listagem",
+					icon: HiDocumentText,
+				},
+				{
+					label: "Cadastro",
+					href: "/portal/empresas/cadastro",
+					icon: HiCloudUpload,
+				},
+			],
+		},
+	];
 	return (
 		<section>
 			<Layout sideBarItems={sideItems}>
